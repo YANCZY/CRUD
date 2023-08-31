4a8c15e502720076b5c101efa14bb0b5e0675c2f
Yancy Mejia <mckhelyancy.gmail.com>
2023-08-31 15:54:36 +0800

    OOP Modals

---
+++ view/modal/add_user_form.php
@@ -0,0 +1,42 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add User</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Name</label>
+                        <input type="text" class="form-control addName" id="recipient-name" placeholder="Mikey">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control addEmail" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exampl@test.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Username:</label>
+                        <input type="text" class="form-control addUsername" id="recipient-name" placeholder="mikey">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Password:</label>
+                        <input type="password" class="form-control addPassword" id="recipient-name" placeholder="12*">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Mobile:</label>
+                        <input type="text" class="form-control addMobile" id="recipient-name" placeholder="+639552..">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Phone:</label>
+                        <input type="text" class="form-control addPhone" id="recipient-name" placeholder="521-8554">
+                    </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="closecreate">Close</button>
+                    <button type="button" class="btn btn-primary adduser" id="adduser">Submit</button>
+                </div>
+                </div>
+            </div>
+            </div>
---
+++ view/modal/viewdetails_user_form.php
@@ -0,0 +1,42 @@
+<div class="modal fade" id="viewDeatails" tabindex="-1" aria-labelledby="viewDeatails" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="viewDeatails">View Details</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                    <input type="hidden" class="userIdToUpdate" id="userIdToUpdate" name="userIdToUpdate">
+                    <div class="mb-3">
+                        <label for="recipient-name" class="col-form-label">Name</label>
+                        <input type="text" class="form-control addName" id="recipient-name" placeholder="Mikey" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control addEmail" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exampl@test.com" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Username:</label>
+                        <input type="text" class="form-control addUsername" id="recipient-name" placeholder="mikeyusername" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Password:</label>
+                        <input type="password" class="form-control addPassword" id="recipient-name" placeholder="12*" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Mobile:</label>
+                        <input type="text" class="form-control addMobile" id="recipient-name" placeholder="+639552.." disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label">Phone:</label>
+                        <input type="text" class="form-control addPhone" id="recipient-name" placeholder="521-8554" disabled>
+                    </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="closecreate">Close</button>
+                </div>
+                </div>
+            </div>
+            </div>
