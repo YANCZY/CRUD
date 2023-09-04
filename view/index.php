46ea6382363570be848563dbbcd3f34400bd03d8
YANCZY <mckhelyancy@gmail.com>
2023-09-04 18:17:27 +0800

    Remove Close Button in Table

--- view/index.php
+++ view/index.php
@@ -185,9 +185,6 @@
                                     </table>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            </div>
                         </div>
                     </div>
                 </div>
